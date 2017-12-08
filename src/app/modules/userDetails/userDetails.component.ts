01022d7a3cd5c84c7cd4bdde227dc3e8ae8a1120
Jose E. Gonzalez Modecir <jose.enrique.gonzalez.modecir@sap.com>
2017-12-08 11:40:14 +0100

    Added avatar to user details page

--- src/app/modules/userDetails/userDetails.component.ts
+++ src/app/modules/userDetails/userDetails.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 
 import { DataService } from '../../services/data.service';
 
@@ -12,7 +13,7 @@ export class UserDetailsComponent {
     user: any;
     componentSlug: string = '';
 
-    constructor(private dataService: DataService, private route: ActivatedRoute) { }
+    constructor(private dataService: DataService, private route: ActivatedRoute, private sanitizer: DomSanitizer) { }
 
     ngOnInit(): void {
         this.componentSlug = this.route.snapshot.paramMap.get('componentSlug');
@@ -25,6 +26,7 @@ export class UserDetailsComponent {
         this.dataService.getUser(this.route.snapshot.paramMap.get('id')).subscribe(data => {
             setTimeout(()=>{ 
                 this.user = data; 
+                this.user.avatarStyle = this.sanitizer.bypassSecurityTrustStyle('background-image: url("https://api.adorable.io/avatars/240/' + this.user.email + '");');
             }, 500)
         });
     }
